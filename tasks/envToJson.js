d8459312b1bd3afc7d0864118e005a96d14f9884
Ollie Edwards <olliee@mobbu.com>
2014-05-08 00:37:00 +0100

    First working iteration

--- tasks/envToJson.js
+++ tasks/envToJson.js
@@ -8,43 +8,67 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    var path = require('path');
+    var _ = require('lodash');
 
-  grunt.registerMultiTask('envToJson', 'Update json file with environment variable values', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+
+    grunt.registerMultiTask('envToJson', 'Update json file with environment variable values', function () {
+        console.log("running %s", this.name);
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+
+        });
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else  {
+                    return true;
+                }
+            }).map(function (filepath) {
+                // Read file source.
+                var modulePath = path.resolve(filepath);
+                console.log('requiring %s', modulePath);
+                var js = require(modulePath);
+                grunt.file.write(f.dest, JSON.stringify(updateValues(js, options.vars)));
+                grunt.log.writeln('File "' + f.dest + '" created.');
+            });
+        });
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    var updateValues = function(js, vars) {
+        console.log("update object with ", vars);
+        _.each(vars, function(path, key) {
+            var val = process.env[key];
+            if (val) {
+                js = updateValue(js, path.split('.'), val) || js;
+            }
+        });
+        return js;
+    };
+
+    var updateValue = function(object, segments, val) {
+        if (segments.length) {
+            var segment = segments.shift();
+            if (segments.length === 0) {
+                object[segment] = val;
+                return object;
+            } else {
+                if (!_.has(object, segment)) {
+                    object[segment] = {};
+                }
+                object[segment] = updateValue(object[segment], segments, val);
+                return object;
+            }
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+    }
 
 };
